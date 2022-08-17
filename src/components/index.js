0adf84fc11e1ad65a3b53184745655da0b39ab42
Sam <wtawonlife@gmail.com>
2022-08-18 03:03:35 +0900

    🎨모바일/pc 최적화 반응형 화면 구현

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Hero from "./Hero";
+import NavBar from "./NavBar";
+import Billing from "./Billing";
+import Business from "./Business";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import FeedbackCard from "./FeedbackCard";
+import GetStarted from "./GetStarted";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+import Footer from "./Footer";
+import Button from "./Button";
+
+export {
+  Hero,
+  NavBar,
+  Billing,
+  Business,
+  CardDeal,
+  Clients,
+  CTA,
+  FeedbackCard,
+  GetStarted,
+  Stats,
+  Testimonials,
+  Footer,
+  Button,
+};
